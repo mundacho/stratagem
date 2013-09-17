cb87e1af60dab8609db6c7125ad157a48da7fa7f
Edmundo LOPEZ BOBEDA <edmundo.lopez@unige.ch>
2013-09-17 17:31:35 +0200

    Added caching as trait.

---
+++ src/main/scala/ch/unige/cui/smv/sigmadd/impl/basic/LatticeWithCache.scala
@@ -0,0 +1,29 @@
+package ch.unige.cui.smv.sigmadd.impl.basic
+
+import ch.unige.cui.smv.stratagem.sigmadd.LatticeElement
+import scala.collection.mutable.HashMap
+
+trait LatticeWithCache extends LatticeElement {
+
+  type LatticeElementType <: LatticeWithCache
+
+  val operationCache = new HashMap[(String, LatticeWithCache, LatticeWithCache), LatticeElementType]
+
+  abstract override def v(that: LatticeElementType): LatticeElementType = {
+    // order the parameters
+    val key = if (this.hashCode > that.hashCode) ("union", this, that) else ("union", that, this)
+    operationCache.getOrElseUpdate(key, super.v(that))
+  }
+
+  abstract override def ^(that: LatticeElementType): LatticeElementType = {
+    // order the parameters
+    val key = if (this.hashCode > that.hashCode) ("inter", this, that) else ("inter", that, this)
+    operationCache.getOrElseUpdate(key, super.^(that))
+  }
+
+  abstract override def \(that: LatticeElementType): LatticeElementType = {
+    val key = ("diff", this, that)
+    operationCache.getOrElseUpdate(key, super.v(that))
+  }
+
+}
